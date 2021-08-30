43cc21b7fcc7f949b6a94b58b34eee721a383412
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-08-30 16:30:42 -0400

    creating tests in Intern.test.js for Intern class key values and return values in each function

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+test('Can set Intern class school key value via constructor', () => {
+    const testVal = 'GT';
+    const test = new Intern('Foo', 4, 'someone@ex.com', testVal);
+    expect(test.school).toBe(testVal);
+});
+
+test('Should return "Intern" via getRole()', () => {
+    const val = 'Intern';
+    const test = new Engineer('Foo', 4, 'someone@ex.com', 'GT');
+    expect(test.getRole()).toBe(val);
+});
+
+test("Should return Intern's school via getSchool()", () => {
+    const val = 'GT';
+    const test = new Intern('Foo', 4, 'someone@ex.com', val);
+    expect(test.getSchool()).toBe(val);
+});
