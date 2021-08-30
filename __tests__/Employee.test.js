ccdb4381e1e7969cbdacd1314f705b4e62615827
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-08-30 16:02:47 -0400

    finishing Employee.test.js by creating tests for each employee object function return values

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,9 +7,9 @@ test('Should embody Employee instance', () => {
 });
 
 test('Should set name of employee object', () => {
-    const name = 'Francis';
-    const test = new Employee(name);
-    expect(test.name).toBe(name);
+    const n = 'Francis';
+    const test = new Employee(n);
+    expect(test.name).toBe(n);
 });
 
 test('Should set id of employee object', () => {
@@ -22,4 +22,28 @@ test('Should set email of employee object', () => {
     const testEmail = 'someone@example.com';
     const test = new Employee('Foo', 4, testEmail);
     expect(test.email).toBe(testEmail);
+});
+
+test('Should return name via getName()', () => {
+    const n = 'Francis';
+    const test = new Employee(n);
+    expect(test.getName()).toBe(n);
+});
+
+test('Should return id via getId()', () => {
+    const val = 50;
+    const test = new Employee('Foo', val);
+    expect(test.getId()).toBe(val);
+});
+
+test('Should return email via getEmail()', () => {
+    const testEmail = 'someone@example.com';
+    const test = new Employee('Foo', 4, testEmail);
+    expect(test.getEmail()).toBe(testEmail);
+});
+
+test('Should return "Employee" via getRole()', () => {
+    const value = 'Employee';
+    const test = new Employee('Foo', 4, 'someone@example.com');
+    expect(test.getRole()).toBe(value);
 });
